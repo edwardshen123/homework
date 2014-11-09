71f5a8d148e281b0bcca10c86db9d3d247e4e753
edwardshen123 <edwardshen123@gmail.com>
2014-11-09 18:42:50 -0500

    List array

--- 15-Sarray/Sarray.java
+++ 15-Sarray/Sarray.java
@@ -1,53 +1,82 @@
 public class Sarray {
 
     private Object[] data;
-    private int location;
+    private int last;
 
     public Sarray() {
 	data = new Object[10];
-	location = 0;
+	last = 0;
     }
 
-    public void addToEnd(int item) {
+    public boolean add(int item) {
 	if (isArrayOverflow()) {
 	    arrayGrowth();
 	}
-	set(location, item);
+	set(last, item);
+	return true;
     }
-    public void addAtLoc(int index, int item) {
-    }
-    public int get(int index) {
+    public String add(int index, int item) {
 	if (isArrayOverflow(index)) {
-	    //returns error
+	    return "Out of Bound Error";
+	} else {
+	    arrayGrowth();
+	    for (int location = data.length - 1; location > index; location--) {
+		data[location] = data[location - 1];
+	    }
+	    set(index, item);
+	    return "Added " + item + " to " + index;
+	}
+    }
+    public Object get(int index) {
+	if (isArrayOverflow(index)) {
+	    return "Out of Bound Error";
 	} else {
 	    return data[index];
 	}
     }
-    public void set(int index, int item) {
+    public String set(int index, int item) {
 	if (isArrayOverflow(index)) {
-	    //returns error
+	    return "Out of Bound Error";
 	} else {
 	    data[index] = item;
-	    locationShift(index)
+	    lastShift(index);
+	    return "set " + index + " to " + item;
 	}
     }
     public int size() {
 	return data.length;
     }
-    public void remove(int index) {
-    }
-    public void locationShift(int index) {
-	if (index > location) {
-	    location = index + 1;
+    public Object remove(int index) {
+	if (isArrayOverflow(index)) {
+	    //return error
+	    return -1;
 	} else {
-	    location++;
+	    Object item = data[index];
+	    Object[] temp = new Object[data.length - 1];
+	    int dataCounter = 0;
+	    for (int counter = 0; counter < temp.length; counter++) {
+		if (counter == index) {
+		    dataCounter++;
+		}
+		temp[counter] = data[dataCounter];
+		dataCounter++;
+	    }
+	    data = temp;
+	    return item;
+	}
+    }
+    public void lastShift(int index) {
+	if (index > last) {
+	    last = index + 1;
+	} else {
+	    last++;
 	}
     }
     public boolean isArrayOverflow() {
-	return (location >= data.length);
+	return (last >= data.length);
     }
     public boolean isArrayOverflow(int index) {
-	return (index >= data.length);
+	return (index >= data.length || index < 0);
     }
     public void arrayGrowth() {
 	Object[] temp = new Object[data.length + 1];
@@ -59,6 +88,18 @@ public class Sarray {
 	    newArray[c] = data[c];
 	}
     }
-    public void copy(Object[] newArray, int indexException) {
+
+    public static void main(String[] args) {
+	Sarray list = new Sarray();
+	System.out.println(list.add(1));
+	System.out.println(list.add(2));
+	System.out.println(list.add(3));
+	System.out.println(list.add(11, 4));
+	System.out.println(list.set(4, 3));
+	System.out.println(list.set(2, 4));
+	System.out.println(list.size());
+	System.out.println(list.get(2));
+	System.out.println(list.get(5));
+	System.out.println(list.get(11));
     }
 }
