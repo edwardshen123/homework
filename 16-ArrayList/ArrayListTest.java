008aa776f71e3c192d114c32b921251f68058af7
edwardshen123 <edwardshen123@gmail.com>
2014-11-12 19:02:40 -0500

    Added remove adjacent

--- 16-ArrayList/ArrayListTest.java
+++ 16-ArrayList/ArrayListTest.java
@@ -3,17 +3,21 @@ import java.util.Random;
 public class ArrayListTest {
     public void removeAdj(ArrayList al) {
 	for (int c = 0; c < al.size(); c++) {
-	    while (al.get(c) == al.get(c++)) {
-		al.remove(c++);
+	    while (c < al.size() - 1 && al.get(c) == al.get(c + 1)) {
+		al.remove(c + 1);
 	    }
 	}
     }
     public static void main(String[] args) {
 	ArrayListTest test = new ArrayListTest();
 	ArrayList<Integer> al = new ArrayList<Integer>();
+	System.out.println(al.size());
 	Random rand = new Random();
-	for (int c = 0; c < al.size(); c++) {
+	for (int c = 0; c < 20; c++) {
+	    al.add(rand.nextInt(10));
 	}
-	
+	System.out.println(al);
+	test.removeAdj(al);
+	System.out.println(al);
     }
 }
