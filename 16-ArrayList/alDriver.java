e8afddf7966692ecaef4a9f1a2d3cd5289b15669
edwardshen123 <edwardshen123@gmail.com>
2014-11-13 20:53:56 -0500

    updated

--- 16-ArrayList/alDriver.java
+++ 16-ArrayList/alDriver.java
@@ -4,29 +4,48 @@ import java.util.Arrays;
 
 public class alDriver {
 
-    public void randomize(ArrayList<Integer> n, char type, Random r) {
-	//Time Efficient (ArrayList)
-	if (type == 't') {
-	    for (int c = 0; c < n.size(); c++) {
-		n.add(r.nextInt(n.size() - c), n.remove(n.size()/2));
-	    }
-	}
-	//Space Efficient (ArrayList)
-	if (type == 's') {
-	    for (int c = 0; c < n.size(); c++) {
-		n.add(r.nextInt(n.size() - c), n.remove(n.size()/2));
+    private Random rand = new Random();
+    
+    public void randomize(ArrayList<Integer> n) {
+	int loc = n.size()/2;
+	//Time & Space Efficient (ArrayList)
+	for (int c = 0; c < n.size(); c++) {
+	    int next = rand.nextInt(n.size());
+	    if (next == loc && n.size != 1) {
+		n.add(next - 1, n.remove(loc));
+	    } else {
+		n.add(next, n.remove(loc));
 	    }
 	}
     }
     public void randomize(int[] n, char type) {
 	//Time Efficient (Array)
-	//Space Efficient (ArrayList)
+	if (type == 't') {
+	    for (int counter = 0; counter < n.length/2; counter++) {
+		int locONE = rand.nextInt(n.length);
+		int locTWO = rand.nextInt(n.length);
+		if (locONE == locTWO) {
+		    if (locONE == 0) {
+			locONE++;
+		    } else {
+			locONE--;
+		    }
+		}
+		int valONE = n[locONE];
+		int valTWO = n[locTWO];
+		n[locONE] = valTWO;
+		n[locTWO] = valONE;
+	    }
+	}
+	//Space Efficient (Array)
+	if (type == 's') {
+	    
+	}
     }
     public static void main(String[] args) {
-	Random rand = new Random();
 	alDriver controller = new alDriver();
 	
-	int[] arry = new int[1000000];
+	int[] arry = new int[10];
 	ArrayList<Integer> arryList = new ArrayList<Integer>();
 	for (int c = 0; c < arry.length; c++) {
 	    arry[c] = c;
