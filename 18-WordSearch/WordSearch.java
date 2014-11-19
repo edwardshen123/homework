49b293938693767eceba9e7135dd622b0b47c923
edwardshen123 <edwardshen123@gmail.com>
2014-11-19 18:28:35 -0500

    added horizontal words

---
+++ 18-WordSearch/WordSearch.java
@@ -0,0 +1,74 @@
+import java.lang.StringBuilder;
+/**
+ * Creates a word search puzzle
+ *
+ */
+public class WordSearch{
+
+    private char[][] board;
+    private int boardRow;
+    private int boardCol;
+
+    public WordSearch(int r, int c){
+	board = new char[r][c];
+	boardRow = r;
+	boardCol = c;
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		board[i][j]='.';
+	    }
+	}			
+    }
+
+    public WordSearch(){
+	this(20,30);
+    }
+
+    public String toString(){
+	String s = "";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		s = s + board[i][j];
+	    }
+	    s = s + "\n";
+	}
+	return s;
+    }
+
+    public void addWordH(String w,int row, int col){
+	int len = w.length();
+	if (col+len<boardCol && col>=0 && row<boardRow && row>=0) {
+	    int r = row, c = col;
+	    boolean canAdd = true;
+	    for (int counter=0; counter<len; counter++) {
+		if (board[r][c+counter] != '.') {
+		    canAdd = false;
+		    break;
+		}
+	    }
+	    if (canAdd) {
+		for (int i=0;i<len;i++){
+		    board[r][c+i] = w.charAt(i);
+		}
+	    }
+	}
+    }
+
+    public void addWordHR(String w, int row, int col) {
+	String reverse = new StringBuilder(w).reverse().toString();
+	addWordH(reverse, row, col);
+    }
+
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordH("hello",3,5);
+	w.addWordH("look",3,8);
+	w.addWordH("look",3,5);
+	w.addWordH("look",2,5);
+	w.addWordH("hello",100,5);
+	w.addWordH("hello",30,555);
+				
+	System.out.println(w);
+    }
+}
