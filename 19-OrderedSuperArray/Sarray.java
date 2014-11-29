3c84aa5697454f947a15052cddba82850f95c2f9
edwardshen123 <edwardshen123@gmail.com>
2014-11-29 12:04:19 -0500

    make superArray for String

---
+++ 19-OrderedSuperArray/Sarray.java
@@ -0,0 +1,109 @@
+public class Sarray {
+
+    private Object[] data;
+    private int last;
+
+    public Sarray() {
+	data = new Object[10];
+	last = 0;
+    }
+
+    public boolean add(String item) {
+	if (isArrayOverflow()) {
+	    arrayGrowth();
+	}
+	set(last, item);
+	return true;
+    }
+    public void add(int index, String item) {
+	if (isArrayOverflow(index)) {
+	    throw new ArrayIndexOutOfBoundsException();
+	} else {
+	    arrayGrowth();
+	    for (int location = data.length - 1; location > index; location--) {
+		data[location] = data[location - 1];
+	    }
+	    set(index, item);
+	}
+    }
+    public Object get(int index) {
+	if (isArrayOverflow(index)) {
+	    throw new ArrayIndexOutOfBoundsException();
+	} else {
+	    return data[index];
+	}
+    }
+    public void set(int index, String item) {
+	if (isArrayOverflow(index)) {
+	    throw new ArrayIndexOutOfBoundsException();
+	} else {
+	    data[index] = item;
+	    lastShift(index);
+	}
+    }
+    public int size() {
+	return data.length;
+    }
+    public Object remove(int index) {
+	if (isArrayOverflow(index)) {
+	    throw new ArrayIndexOutOfBoundsException();
+	} else {
+	    Object item = data[index];
+	    Object[] temp = new Object[data.length - 1];
+	    int dataCounter = 0;
+	    for (int counter = 0; counter < temp.length; counter++) {
+		if (counter == index) {
+		    dataCounter++;
+		}
+		temp[counter] = data[dataCounter];
+		dataCounter++;
+	    }
+	    data = temp;
+	    last--;
+	    return item;
+	}
+    }
+    public void lastShift(int index) {
+	if (index > last) {
+	    last = index + 1;
+	} else {
+	    last++;
+	}
+    }
+    public boolean isArrayOverflow() {
+	return (last >= data.length);
+    }
+    public boolean isArrayOverflow(int index) {
+	return (index >= data.length || index < 0);
+    }
+    public void arrayGrowth() {
+	Object[] temp = new Object[data.length + 1];
+	copy(temp);
+	data = temp;
+    }
+    public void copy(Object[] newArray) {
+	for (int c = 0; c < data.length; c++) {
+	    newArray[c] = data[c];
+	}
+    }
+
+    public static void main(String[] args) {
+	Sarray list = new Sarray();
+	try {
+	System.out.println(list.add("hi"));
+	System.out.println(list.add("there"));
+	System.out.println(list.add("world"));
+	list.add(9, "no");
+	list.set(4, "more");
+	list.set(2, "numbers");
+	System.out.println(list.size());
+	System.out.println(list.get(2));
+	System.out.println(list.get(5));
+	System.out.println(list.get(9));
+	} catch (ArrayIndexOutOfBoundsException e) {
+	    System.out.println(e);
+	} catch (Exception e) {
+	    System.out.println(e);
+	}
+    }
+}
