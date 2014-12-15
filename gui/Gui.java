e0c6a7aceaf2105301a36cc5c8506a78c754e8db
edwardshen123 <e@eshen.me>
2014-12-15 08:39:46 -0500

    updated

---
+++ gui/Gui.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+
+    public Gui() {
+	setTitle("My First Gui");
+	setSize(600, 400);
+	setLocation(100, 100);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+	Gui g = new Gui();
+	g.setVisible(true);
+    }
+}
